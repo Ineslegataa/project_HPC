2fd7225f354fc3e7de3076e46f592950d06ba036
Ines el Gataa <elgataaines@gmail.com>
2024-05-12 22:10:48 +0200

    Adding class for mandelbrot set

--- assignment_2/2c/main.cpp
+++ assignment_2/2c/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
+class Mandelbrot_set {
+private:
+    // aggiungere valori di default
+    unsigned int n_x, n_y;
+    short int I_max=0;
+    double x_L, y_L, x_R, y_R;
+    double delta_x,delta_y;
+
+public:
+    Mandelbrot_set(unsigned int nx, unsigned int ny, double xl, double yl, double xr, double yr, short int imax)
+        : n_x(nx), n_y(ny), x_L(xl), y_L(yl), x_R(xr), y_R(yr), I_max(imax) {initialize_grid();}
+
+    void display_parameters() const {
+        std::cout << "Resolution: " << n_x << "x" << n_y << std::endl;
+        std::cout << "Bounds: [" << x_L << ", " << y_L << "] to [" << x_R << ", " << y_R << "]" << std::endl;
+        std::cout << "Maximum iterations: " << I_max << std::endl;
+    }
+
+};
+
 int main(int argc, char** argv) {
     unsigned int n_x, n_y;
     short int I_max;
@@ -17,6 +37,9 @@ int main(int argc, char** argv) {
     else {
         std::cerr << "Incorrect number of parameters. Expected 7 parameters." << std::endl;
     }
+
+    Mandelbrot_set mandelbrot_set(n_x,n_y,x_L,y_L,x_R,y_R,I_max);
+    mandelbrot_set.display_parameters();
 
     return 0;
 }
